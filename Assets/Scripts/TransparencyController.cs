a2bcf30c9b5a45e381d28bd194af4193bf749fbb
Bentleyj <jgbentley10@gmail.com>
2016-08-08 15:33:12 +0100

    Added color to Transparency Controller

--- Assets/Scripts/TransparencyController.cs
+++ Assets/Scripts/TransparencyController.cs
@@ -6,7 +6,12 @@ public class TransparencyController : MonoBehaviour {
     public Renderer rend;
     // Use this for initialization
     [Range(0, 1)]
+    public float saturation;
+    [Range(0, 1)]
+    public float hue;
+    [Range(0, 1)]
     public float alpha;
+    public bool blocking = false;
 	void Start () {
         if (!rend)
             rend = GetComponent<MeshRenderer>();
@@ -14,6 +19,14 @@ public class TransparencyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        rend.material.SetColor("_Color", new Color(alpha, alpha, alpha));// = new Color(1.0f, 1.0f, 1.0f, alpha);
-	}
+        Color col = Color.HSVToRGB(hue, saturation, 1.0f);
+        if(blocking)
+        {
+            rend.material.SetColor("_Color", new Color(col.r, col.g, col.b, alpha));// = new Color(1.0f, 1.0f, 1.0f, alpha);
+        }
+        else
+        {
+            rend.material.SetColor("_Color", new Color(col.r * alpha, col.g * alpha, col.b * alpha));// = new Color(1.0f, 1.0f, 1.0f, alpha);
+        }
+    }
 }
